2b8bba7864d25d230dc2e537a0e8ad2a6f07586d
Benjamin Dionysus <benjamin@sixbynine.com>
2020-11-01 15:42:29 -0600

    JawsDB Config

--- config/connections.js
+++ config/connections.js
@@ -1,12 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: ")rWh0",
-  database: "burgers_db"
+  database: "burger_db"
 });
 
 // Make connection.
